1153f391e88765f610d311ee9a223abe90ba01f5
huyan <475616043@qq.com>
2016-09-21 17:17:32 +0800

    up

--- src/main/java/com/learn/ex06/pyrmont/core/SimpleContextLifecycleListener.java
+++ src/main/java/com/learn/ex06/pyrmont/core/SimpleContextLifecycleListener.java
@@ -3,6 +3,8 @@ package com.learn.ex06.pyrmont.core;
 import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleEvent;
 import org.apache.catalina.LifecycleListener;
+import org.apache.catalina.loader.WebappClassLoader;
+import org.apache.catalina.loader.WebappLoader;
 
 /**
  * Created by huyan on 2016/9/20.
@@ -19,5 +21,6 @@ public class SimpleContextLifecycleListener implements LifecycleListener {
         } else if (Lifecycle.STOP_EVENT.equals(event.getType())){
             System.out.println("Stop event");
         }
+
     }
 }
---
+++ src/main/java/com/learn/ex08/pyrmont/core/SimpleContextConfig.java
@@ -0,0 +1,22 @@
+package com.learn.ex08.pyrmont.core;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+/**
+ * Created by huyan on 2016/9/21.
+ */
+public class SimpleContextConfig implements LifecycleListener {
+
+    @Override
+    public void lifecycleEvent(LifecycleEvent event) {
+
+        if (Lifecycle.START_EVENT.equals(event.getType())){
+
+            Context context = (Context)event.getLifecycle();
+            context.setConfigured(true);
+        }
+    }
+}
